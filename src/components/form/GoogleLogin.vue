d941b6ce6ac33573a926e25b648ed7bcf6886ffc
Ali Karam <AK@alikaram.com>
2022-07-16 17:29:56 +0300

    style: fix button colors

--- src/components/form/GoogleLogin.vue
+++ src/components/form/GoogleLogin.vue
@@ -1,6 +1,8 @@
 <template>
-  <button class="mb-2 w-full rounded bg-red-700 p-2" @click="signInWithGoogle">
-    <span> <!-- insert google logo svg with 24 by 24 size --> </span>Sign In
+  <button class="mb-2 w-full rounded bg-red-700 p-2 text-white" @click="signInWithGoogle">
+    <span>
+      <!-- insert google logo svg with 24 by 24 size -->
+    </span>Sign In
     with Google
   </button>
 </template>
--- src/components/form/LoginForm.vue
+++ src/components/form/LoginForm.vue
@@ -27,8 +27,7 @@ function login() {
       :validation-schema="schema"
       data-test="form"
       @submit="login"
-      @keyup.enter="login"
-    >
+      @keyup.enter="login">
       <div class="form-group flex flex-col">
         <label for="email" class="">Email Address</label>
         <Field
@@ -37,10 +36,9 @@ function login() {
           name="email"
           type="text"
           placeholder="email address"
-          class="mb-2 rounded border border-slate-800 p-2 text-slate-800"
-        />
+          class="mb-2 rounded border border-slate-800 p-2 text-slate-800" />
         <span class="mb-2 text-sm capitalize text-red-600">{{
-          errors.email
+            errors.email
         }}</span>
       </div>
       <div class="form-group mb-2 flex flex-col">
@@ -60,8 +58,7 @@ function login() {
               border border-slate-800
               p-2
               text-slate-800
-            "
-          />
+            " />
           <div class="absolute inset-y-2 right-0 pr-3">
             <div class="flex items-center" @click="toggle = !toggle">
               <EyeIcon v-if="toggle" class="h-6 w-6 text-slate-800" />
@@ -71,14 +68,13 @@ function login() {
         </div>
 
         <span class="mb-2 text-sm capitalize text-red-600">{{
-          errors.password
+            errors.password
         }}</span>
       </div>
       <button
-        class="mb-2 w-full rounded bg-slate-700 p-2"
+        class="mb-2 w-full rounded bg-slate-700 p-2 text-white"
         type="submit"
-        :disabled="loading.value"
-      >
+        :disabled="loading.value">
         {{ loading.value ? 'Logging in...' : 'Login' }}
       </button>
     </Form>
